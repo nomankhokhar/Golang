4caaeac1b2c12ecf3022d948ce5da8630826c872
nomankhokhar <54282116+nomankhokhar@users.noreply.github.com>
2023-08-12 05:55:41 +0500

    More Practice Code of Golang

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 const (
 	lol           string = "Noman"
@@ -14,6 +12,12 @@ const (
 	j
 	k
 )
+
+type Student struct {
+	name     string
+	RollNo   int
+	subjects []string
+}
 
 func main() {
 	// var i int = 12
@@ -62,12 +66,124 @@ func main() {
 	// 	fmt.Println(TwoDArray)
 	// 	fmt.Println(Slice)
 
-	var a []int = make([]int, 3)
-	a[0] = 4
-	a[1] = 14
-	a[2] = 44
+	// var a []int = make([]int, 3)
+	// a[0] = 4
+	// a[1] = 14
+	// a [2] = 44
 
-	var b []int = append(a, 5)
-	fmt.Println(len(a))
-	fmt.Println(b)
+	// var b []int = append(a, 5)
+	// fmt.Println(len(a))
+	// fmt.Println(b)
+
+	shopinCart := map[string]int{
+		"KeyBoard": 100,
+		"Mouse":    50,
+		"Laptop":   1000,
+	}
+
+	for k, v := range shopinCart {
+		fmt.Println(k, v)
+	}
+
+	// _, ok := shopinCart["KeyBoard"]
+	// if ok {
+	// 	fmt.Println("Avail")
+	// }
+
+	// if _, ok := shopinCart["KeyBoard"]; ok {
+	// 	fmt.Println("Avail")
+	// }
+
+	// shopinCart["Book"] = 30
+	// has, ok := shopinCart["Book"]
+
+	// // Books will get ByRefrence address of Shoping
+	// books := shopinCart
+
+	// fmt.Println(has, ok)
+	// fmt.Println(shopinCart["Mouse"])
+	// delete(books, "Book")
+
+	// fmt.Println(shopinCart)
+
+	// Structure of Struct
+
+	// student := Student{
+	// 	"Noman Ali",
+	// 	1242,
+	// 	[]string{
+	// 		"Maths",
+	// 		"Physics",
+	// 		"Chemistry",
+	// 	},
+	// }
+
+	// NomanAli := Student{
+	// 	name:   "Noman Ali",
+	// 	RollNo: 1242,
+	// 	subjects: []string{
+	// 		"Maths",
+	// 		"Physics",
+	// 		"Chemistry",
+	// 	},
+	// }
+
+	// NomanAli.name = "Noman Khan"
+	// fmt.Println(NomanAli)
+	// fmt.Println(student)
+	// fmt.Println(student.name)
+	// fmt.Println(student.RollNo)
+	// fmt.Println(student.subjects)
+
+	// Pc := Computer{}
+
+	// Pc.cores = 12
+	// Pc.memoryCapacity = 1000
+	// Pc.memoryType = "SSD"
+	// Pc.price = 1212
+	// Pc.processorName = "NomanAli"
+
+	// fmt.Println(Pc)
+	// fmt.Println(Pc.Memory)
+	// fmt.Println(Pc.Processor)
+
+	// com := Computer{
+	// 	Processor: Processor{
+	// 		processorName: "NomiBhai",
+	// 		cores:         12,
+	// 	},
+	// 	Memory: Memory{
+	// 		memoryCapacity: 12121,
+	// 		memoryType:     "SSD",
+	// 	},
+	// 	price: 124234,
+	// }
+
+	// fmt.Println(com)
+
+	// bla := []int{1, 2, 3, 4, 45, 6, 54, 7, 7}
+
+	// for _, v := range bla {
+	// 	fmt.Println(v)
+	// }
+
+	// for i, j := 0, 0; i < 5; i, j = i+1, j+1 {
+	// 	fmt.Println(i, j)
+	// }
+}
+
+type Processor struct {
+	processorName string
+	cores         int
+}
+
+type Memory struct {
+	memoryCapacity int
+	memoryType     string
+}
+
+type Computer struct {
+	Processor
+	Memory
+	price int
 }
