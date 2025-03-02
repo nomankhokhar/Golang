48678710e31d9784bc865743eb020dfe5653bee8
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2025-03-03 00:31:38 +0500

    Write my Own Straming Read func

--- Golang With Cloud Integration/1 Go Application/main.go
+++ Golang With Cloud Integration/1 Go Application/main.go
@@ -1,17 +1,81 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
+
+// JSON to Struct Golang
+type Words struct {
+	Page  string   `json:"page"`
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
+
+type MySlowReader struct {
+	contents string
+	pos      int
+}
+
+func (m *MySlowReader) Read(p []byte) (n int, err error) {
+	if m.pos+1 <= len(m.contents) {
+		n := copy(p, m.contents[m.pos:m.pos+1])
+		m.pos++
+		return n, nil
+	}
+	return 0, io.EOF
+}
 
 func main() {
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Println("Usage: ./hell-world <argument>\n")
+		fmt.Println("Usage: ./http-get <argument>")
 		os.Exit(1)
 	}
 
-	fmt.Println("Hello Cloud", args[1:])
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	mySlowReaderInstance := &MySlowReader{
+		contents: "Hello lol xD",
+	}
+	out, _ := io.ReadAll(mySlowReaderInstance)
+
+	fmt.Println("Custome Struct and Interface", out)
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response.StatusCode != 200 {
+		fmt.Printf("HTTP Status Code: %d\n Body: %s", response.StatusCode, body)
+		os.Exit(1)
+	}
+
+	var words Words
+
+	// convert request body data into Go Struct
+	err = json.Unmarshal(body, &words)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON Parsed\n Page: %s\nWords: %v\n", words.Page, strings.Join(words.Words, ", "))
+
 }
