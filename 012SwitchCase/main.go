0945245d61b63ab642282ad8aef78bd13aaace6f
Noman Ali <aliyounas084@gmail.com>
2026-04-07 14:59:04 +0500

    Fix: Switch Stateemnt

--- 012SwitchCase/main.go
+++ 012SwitchCase/main.go
@@ -9,18 +9,27 @@ import (
 func main() {
 	fmt.Println("Welcome to SwitchCase")
 
+	// This seed will ensure that we get a different random number each time
 	rand.Seed(time.Now().UnixNano())
-	diceNumber := rand.Intn(6)+1
-	fmt.Println("Value of dice is ", diceNumber)
+	// This Intn(10) will generate a random number between 0 and 9
+	diceNumber := rand.Intn(10)
 
-
+	fmt.Println("Value of dice is", diceNumber)
 
 	switch diceNumber {
-	case 1: fmt.Println("Value of dice is ", diceNumber)
-	case 2:	fmt.Println("Value of dice is ", diceNumber)
-	case 3:	fmt.Println("Value of dice is ", diceNumber)
-	case 4:	fmt.Println("Value of dice is ", diceNumber)
-	case 5:	fmt.Println("Value of dice is ", diceNumber)
-	case 6:	fmt.Println("Value of dice is ", diceNumber)
+	case 1:
+		fmt.Println("Value of dice is ", diceNumber)
+	case 2:
+		fmt.Println("Value of dice is ", diceNumber)
+	case 3:
+		fmt.Println("Value of dice is ", diceNumber)
+	case 4:
+		fmt.Println("Value of dice is ", diceNumber)
+	case 5:
+		fmt.Println("Value of dice is ", diceNumber)
+	case 6:
+		fmt.Println("Value of dice is ", diceNumber)
+	default:
+		fmt.Println("Default Value of dice is ", diceNumber)
 	}
 }
