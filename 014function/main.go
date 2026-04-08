a5eb59831466d089d2b6cb018c8b0f45ccc80926
Noman Ali <aliyounas084@gmail.com>
2026-04-08 16:29:17 +0500

    Fix the function

--- 014function/main.go
+++ 014function/main.go
@@ -2,42 +2,45 @@ package main
 
 import "fmt"
 
+// iota will increment by 1 for each constant in the block
 const (
-	first = iota + 1
-	second
-	third = 'a'
-	forth
+	first  = iota + 1 // This will add 1 to the iota value
+	second = 65
+	third  = 'a'
+	forth  = 0
 )
 
 func main() {
-	fmt.Println("Welcome to function")
+	fmt.Println("welcome to function")
+
 	Salaam()
-	res := addTwoNum(3, 3)
-	fmt.Println(res)
-
-	fmt.Println(preOrder(1, 2, 3, 4, 5, 6))
-
+	fmt.Println(preOrder(1, 2, 3, 4, 5, 6, 7))
 	fmt.Println(testAdd(1, 2))
-
 	fmt.Println(first, second, third, forth)
 }
 
-func preOrder(value ...int) (string, int) {
-	total := 0
-	for _, val := range value {
-		total += val
-	}
-	return "The total is :", total
-}
-
-func addTwoNum(a int, b int) int {
-	return (a + b)
-}
-
 func Salaam() {
-	fmt.Println("VelKome Sir")
+	fmt.Println("Velkome Sir")
+
+	res := addTwoNum(3, 3)
+	fmt.Println(res)
 }
 
-func testAdd(num1 int, num2 int) (int, error) {
-	return num1 + num1, nil
+func addTwoNum(a, b int) int {
+	return a + b
+}
+
+func preOrder(values ...int) int {
+	Sumvalue := 0
+	for _, value := range values {
+		fmt.Println(value)
+		Sumvalue += value
+	}
+	fmt.Println("Sum:", Sumvalue)
+	return Sumvalue
+}
+
+func testAdd(a, b int) int {
+	fmt.Println(a, b)
+	return a + b
 }
