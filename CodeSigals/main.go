bcf3cee8611bc24d5d9de7ae43518dd23a099a13
Noman Ali <aliyounas084@gmail.com>
2026-06-06 17:31:58 +0500

    Added New Ticket System

--- CodeSigals/main.go
+++ CodeSigals/main.go
@@ -1,67 +1,44 @@
 package main
 
-type StorageManager struct {
-	warehouseQueue  []string
-	inspectionStack []string
+type TicketBookingSystem struct {
+	priorityQueue []string
+	regularQueue  []string
 }
 
-// StoreItem adds item to queue and stack
-func (s *StorageManager) StoreItem(item string) {
-
-	// Add item to stack
-	s.inspectionStack = append(s.inspectionStack, item)
-
-	// Add item to queue
-	s.warehouseQueue = append(s.warehouseQueue, item)
+func (t *TicketBookingSystem) AddCustomer(customer string, isPriority bool) {
+	// TODO: Append the customer to priorityQueue if isPriority is true, otherwise to regularQueue.
+	if isPriority {
+		t.priorityQueue = append(t.priorityQueue, customer)
+		return
+	}
+	t.regularQueue = append(t.regularQueue, customer)
 }
 
-// RetrieveItem gets FIRST item from queue (FIFO)
-func (s *StorageManager) RetrieveItem() *string {
-
-	// Queue is empty
-	if len(s.warehouseQueue) == 0 {
-		return nil
+func (t *TicketBookingSystem) ServeCustomer() (string, bool) {
+	// TODO: Serve first customer from priorityQueue if not empty, otherwise from regularQueue.
+	// Return the customer's name and true on success. If queues are empty, return empty string and false.
+	if len(t.priorityQueue) >= 1 {
+		nameofCus := t.priorityQueue[0]
+		t.priorityQueue = t.priorityQueue[1:]
+		return nameofCus, true
+	}
+	if len(t.regularQueue) >= 1 {
+		nameofCus := t.regularQueue[0]
+		t.regularQueue = t.regularQueue[1:]
+		return nameofCus, true
 	}
 
-	// Get first item
-	firstItem := s.warehouseQueue[0]
-
-	// Remove first item
-	s.warehouseQueue = s.warehouseQueue[1:]
-
-	return &firstItem
+	return "", false
 }
 
-// InspectItem gets LAST item from stack (LIFO)
-func (s *StorageManager) InspectItem() *string {
-
-	// Stack is empty
-	if len(s.inspectionStack) == 0 {
-		return nil
+func (t *TicketBookingSystem) NextCustomer() (string, bool) {
+	// TODO: Return the name of the first customer from priorityQueue if not empty, otherwise from regularQueue.
+	// If queues are empty, return empty string and false.
+	if len(t.priorityQueue) >= 1 {
+		return t.priorityQueue[0], true
 	}
-
-	// Get last item
-	lastItem := s.inspectionStack[len(s.inspectionStack)-1]
-
-	// Remove last item
-	s.inspectionStack = s.inspectionStack[:len(s.inspectionStack)-1]
-
-	return &lastItem
-}
-
-// GetAllItems returns copy of queue
-func (s *StorageManager) GetAllItems() []string {
-
-	// Return empty slice if queue is empty
-	if len(s.warehouseQueue) == 0 {
-		return []string{}
+	if len(t.regularQueue) >= 1 {
+		return t.regularQueue[0], true
 	}
-
-	// Make new slice
-	copyWarehouse := make([]string, len(s.warehouseQueue))
-
-	// Copy data
-	copy(copyWarehouse, s.warehouseQueue)
-
-	return copyWarehouse
+	return "", false
 }
